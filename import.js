251648ebbaf26a98a369df2f674699cc0d8a1614
Karissa McKelvey <krmckelv@gmail.com>
2015-01-16 17:18:07 -0800

    Frontend fixes, add github/author

--- import.js
+++ import.js
@@ -2,12 +2,33 @@ var search = require('sqlite-search')
 var fs = require('fs')
 var ndjson = require('ndjson')
 var through = require('through2')
+var gh = require('github-url-to-object')
 
-search({path: 'npm-readmes.sqlite', columns: ["name", "readme"]}, function(err, db) {
+search({path: 'npm-readmes.sqlite', columns: ["name", "readme", "github", "author"]}, function(err, searcher) {
   if (err) return console.error('err', err)
-  var writer = db.createWriteStream()
-  process.stdin.pipe(ndjson.parse()).pipe(writer)
+  var writer = searcher.createWriteStream()
+  process.stdin.pipe(ndjson.parse()).pipe(filter()).pipe(writer)
   writer.on('finish', function() {
-    db.db.close()
+    searcher.db.close()
   })
 })
+
+function filter() {
+  var stream = through.obj(function(obj, enc, next) {
+    if (obj.name && obj.readme) {
+
+      if (obj.repository && obj.repository.type == 'git') {
+        var ghObj = gh(obj['repository']['url'])
+        if (ghObj) {
+          obj.github = ghObj['https_url']
+        }
+      }
+      if (obj.author) {
+        obj.author = obj['author']['name']
+      }
+      stream.push(obj)
+    }
+    next()
+  })
+  return stream
+}
--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ var Ractive = require('ractive')
 var nets = require('nets')
 var on = require('component-delegate').bind
 var marked = require('marked')
+var qs = require('querystring')
 
-var baseURL = 'http://localhost:8877'
-
+var BASE_URL = 'http://localhost:8877'
+var SEARCH_PATH = '/search/readme/'
 var searchResults = new Ractive({
   el: '.morphsearch-content .dummy-column',
   template: require( '../templates/list.html' ),
@@ -15,14 +16,25 @@ var searchResults = new Ractive({
   }
 })
 
+var itemResult = new Ractive({
+  el: '.morphsearch-content .text-content',
+  template: require( '../templates/text.html' ),
+  data: {
+    name: 'Readme',
+    item: {}
+  }
+})
+
+var ITEM_LOOKUP = {}
+
 var morphSearch = document.getElementById('morphsearch')
 var input = morphSearch.querySelector('input.morphsearch-input')
-var textContent = document.querySelector('.text-content')
 var ctrlClose = morphSearch.querySelector('span.morphsearch-close')
 var isOpen = isAnimating = false
 
 // events
 input.addEventListener('focus', toggleSearch)
+input.addEventListener('keydown', toggleSearch)
 ctrlClose.addEventListener('click', toggleSearch)
 
 // esc key closes search overlay
@@ -34,34 +46,91 @@ document.addEventListener('keydown', function(ev) {
 	}
 })
 
+function addItems (items) {
+  for (i in items) {
+    var item = items[i]
+    ITEM_LOOKUP[item.name] = item
+  }
+  var manymoreitems = searchResults.get('items').concat(items)
+  searchResults.set('items', manymoreitems)
+}
+
 on(document, '.search-result', 'click', function(ev) {
   console.log(ev)
   var name = ev.target.innerText
-  nets({url: baseURL + '/readme/' + encodeURIComponent(name), json: true}, function(err, resp, json) {
-    if (err) alert(err)
-    textContent.innerHTML = marked(json.rows[0].readme)
-  })
+  var item = ITEM_LOOKUP[name]
+  if (!item.marked) {
+    item.marked = true
+    item.readme = marked(item.readme)
+  }
+  itemResult.set('item', item)
   ev.preventDefault()
 })
 
+var next = null
+var currentTerm = null
+
 morphSearch.querySelector('button[type="submit"]').addEventListener('click', function(ev) {
-  var term = input.value
-  nets({url: baseURL + '/search/readme/' + encodeURIComponent(term), json: true}, function(err, resp, json) {
-    if (err) alert(err)
-    searchResults.set('items', json.rows)
-  })
   ev.preventDefault()
+
+  currentTerm = input.value
+  if (!currentTerm) return
+  doSearch(currentTerm, {limit: 50}, function (json) {
+    searchResults.set('items', [])
+    addItems(json.rows)
+  })
 })
+
+function doSearch(term, query, cb) {
+  if (typeof query == 'object') {
+    query = '?' + qs.stringify(query)
+  }
+  nets({
+    url: BASE_URL + SEARCH_PATH + encodeURIComponent(term) + query,
+    json: true
+  }, function(err, resp, json) {
+    if (err) return handleError(err)
+    next = json.next
+    cb(json)
+  })
+}
+
+loading(false)
+
+function loading(val) {
+  searchResults.set('loading', val)
+}
+
+function isLoading() {
+  return searchResults.get('loading')
+}
+
+function fetchMore (cb) {
+  if (!isLoading()) {
+    loading(true)
+    doSearch(currentTerm, next, function (json) {
+      addItems(json.rows)
+      loading(false)
+    })
+  }
+}
+
+function handleError(err) {
+  alert(err)
+}
 
 // show/hide search area
 function toggleSearch(evt) {
 	// return if open and the input gets focused
-	if (evt.type.toLowerCase() === 'focus' && isOpen) return false
+  var eventName = evt.type.toLowerCase()
+	if ((eventName === 'focus' || eventName === 'keydown') && isOpen) return false
 
 	if (isOpen) {
 		classie.remove(morphSearch, 'open')
+    searchResults.set('items', [])
+    itemResult.set('item', null)
 
-		// trick to hide input text once the search overlay closes 
+		// trick to hide input text once the search overlay closes
 		// todo: hardcoded times, should be done after transition ends
 		if (input.value !== '') {
 			setTimeout(function() {
@@ -72,10 +141,23 @@ function toggleSearch(evt) {
 				}, 300)
 			}, 500)
 		}
-		
+
 		input.blur()
 	} else {
 		classie.add(morphSearch, 'open')
 	}
 	isOpen = !isOpen
 }
+
+var scrollTarget = document.getElementsByClassName('dummy-column')[0];
+
+window.onscroll = function (event) {
+  var target = scrollTarget.scrollHeight
+  var scrollPos = window.scrollY + window.innerHeight
+  if (scrollPos >= target) {
+    fetchMore()
+  }
+}
+on(document, '.next', 'click', function(ev) {
+  ev.preventDefault()
+})
